e90653169a3db33fe8178ae115439c15eda5ab54
maxden33@gmail.com <maxden33@gmail.com>
2019-06-12 10:48:35 +0300

    Рефакторинг.

---
+++ src/main/java/ru/magnat/MerchService.java
@@ -0,0 +1,25 @@
+package ru.magnat;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/merch")
+public class MerchService {
+
+    @GET
+    @Path("/ref_user")
+    @Produces(MediaType.APPLICATION_JSON)
+    public RefUser getTrackInJSON() {
+
+        RefUser refUser = new RefUser();
+        refUser.setId("1");
+        refUser.setLogin("egorova.sr@magnat.ru");
+        refUser.setPassword("esr231");
+
+        return refUser;
+
+    }
+
+}
--- src/main/java/ru/magnat/RefUser.java
+++ src/main/java/ru/magnat/RefUser.java
@@ -1,15 +1,38 @@
 package ru.magnat;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-@Path("/ref_user")
 public class RefUser {
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getMessage() {
-        return "Hello world!";
+
+    String id;
+    String login;
+    String password;
+
+    public String getId() {
+        return id;
     }
+
+    public void setId(String title) {
+        this.id = title;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String singer) {
+        this.login = singer;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "RefUser [id=" + id + ", login=" + login + "]";
+    }
+
 }
